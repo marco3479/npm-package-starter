b7ffaf2a3164f7f73eb3269646c0681a8a397227
marco3479 <marcomongaloiii@gmail.com>
2023-01-22 09:58:22 -0500

    test

--- index.ts
+++ index.ts
@@ -8,3 +8,9 @@ export function testing(t: number, u: number, f: number) {
     console.debug('testing');
     return [t, u, f];
 }
+
+type AddFunction = (a: number, b: number) => number
+
+export const add: AddFunction = (a: number, b: number) => {
+    return a + b
+}
