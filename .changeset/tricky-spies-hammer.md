dffe95658662a78697514457649ddd0bcc0e0a1f
marco3479 <marcomongaloiii@gmail.com>
2023-01-22 01:14:46 -0500

    other test

---
+++ .changeset/tricky-spies-hammer.md
@@ -0,0 +1,5 @@
+---
+"pk-components": patch
+---
+
+other test
--- index.ts
+++ index.ts
@@ -2,3 +2,7 @@
 export function test(a: number, b: number) {
     return 'This will be the best package ever!'
 }
+
+export function otherTest() {
+    return true
+}
